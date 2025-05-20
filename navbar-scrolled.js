12680c939332ab6c5d015d36323f9f9cc12d3c40
starkad57 <fabien@uncode-studio.com>
2025-05-20 10:01:51 +0400

    update navbar scrolled
    Add color change for burger

--- navbar-scrolled.js
+++ navbar-scrolled.js
@@ -1,12 +1,14 @@
 const navbar = document.querySelector(".navbar_component");
 const logo = document.querySelector(".navbar_logo-link");
+const lines = document.querySelectorAll(".menu-icon_line-top, .menu-icon_line-middle, .menu-icon_line-bottom");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 20) {
-    navbar.classList.add("navbar-scrolled");
-    logo.classList.add("navbar-scrolled");
-  } else {
-    navbar.classList.remove("navbar-scrolled");
-    logo.classList.remove("navbar-scrolled");
-  }
+  const scrolled = window.scrollY > 20;
+
+  navbar.classList.toggle("navbar-scrolled", scrolled);
+  logo.classList.toggle("navbar-scrolled", scrolled);
+
+  lines.forEach(line => {
+    line.classList.toggle("navbar-scrolled", scrolled);
+  });
 });
